21fd8f2720db401a4a8739f2035343b50f6b8333
Elvira Boman <you@example.com>
2015-09-28 14:49:36 +0200

    Jobbar vidare m OU2 StackPlay II

--- OU2/StackPlay.java
+++ OU2/StackPlay.java
@@ -10,8 +10,7 @@ public class StackPlay {
     * @return the number of calls
     */
    public static long getNumberOfCalls() {
-     return 0;
-     //return move;
+     return move;
    }
    
    /**
@@ -22,15 +21,19 @@ public class StackPlay {
     * @param help the help stack
     * @param n the number of integers to be moved
     */
-   
    public static void move(SpecialStack from, 
                            SpecialStack to, 
                            SpecialStack help, 
-                           int n) 
-   {
-      //move = move + 1; 
+                           int n) {
+     if (n == 1) {
+       to.push(from.pop());
+     } else {
+       move(from, help, to, n-1);
+       to.push(from.pop());
+       move(help, to, from, n-1);
+     }
+     move = move + 1; 
    }
-   
    
    public static void main(String[] args) 
    {
@@ -43,7 +46,7 @@ public class StackPlay {
       System.out.println("   To:   " + to);
       System.out.println("   Help: " + help);
       //System.out.println("   Number of calls: " + getNumberOfCalls());
-      //move(from, to, help, size);
+      move(from, to, help, size);
       System.out.println("End state");
       System.out.println("   From: " + from);
       System.out.println("   To:   " + to);
